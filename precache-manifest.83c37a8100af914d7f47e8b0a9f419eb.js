8600ad8834f3ef6857a04e87a2cc4a3d7234a8cd
Seva Maltsev <sevamaltsev@protonmail.com>
2020-01-03 18:25:11 +0300

    Updated build for gh-pages

---
+++ precache-manifest.83c37a8100af914d7f47e8b0a9f419eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c675f07c30d61d4948e67022aafc0a",
+    "url": "/noita-tools/9040a3ad1e5471028eae.worker.js"
+  },
+  {
+    "revision": "ff55664543f9315fc7a447ca5a045a24",
+    "url": "/noita-tools/index.html"
+  },
+  {
+    "revision": "d8b865ac7851e9ee1b2d",
+    "url": "/noita-tools/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a3bdfceb0ae2599f33e7",
+    "url": "/noita-tools/static/css/main.2615142b.chunk.css"
+  },
+  {
+    "revision": "d8b865ac7851e9ee1b2d",
+    "url": "/noita-tools/static/js/2.4687912e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/noita-tools/static/js/2.4687912e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3bdfceb0ae2599f33e7",
+    "url": "/noita-tools/static/js/main.3d3e471d.chunk.js"
+  },
+  {
+    "revision": "5f2aaa29314af7ddbe3b",
+    "url": "/noita-tools/static/js/runtime-main.b61bfcc0.js"
+  }
+]);
