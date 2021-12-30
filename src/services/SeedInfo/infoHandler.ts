7fced27071f386cc7d7eab07d4aba3069ad4160f
Seva Maltsev <me@seva.dev>
2021-12-30 09:06:53 -0500

    Added explanation

--- src/services/SeedInfo/infoHandler.ts
+++ src/services/SeedInfo/infoHandler.ts
@@ -823,6 +823,10 @@ export class LotteryInfoProvider extends InfoProvider {
   provide(level: number, perkNumber: number, perksOnLevel: number, worldOffset = 0, lotteries = 0) {
     const { x, y } = this.temples[level];
     const perkY = y;
+    // In Noita's code x is `x + (i-0.5)*item_width`
+    // Since we use 0..n-1 instead of 1..n, we use `+ 0.5` since we can
+    // think of this as `i + 1 - 0.5`, which can be simplified to
+    // `i + 0.5`
     const perkX = x + ((perkNumber + 0.5) * (60 / perksOnLevel)) + (35840 * worldOffset);
     const probability = 100 * Math.pow(0.5, lotteries);
     this.randoms.SetRandomSeed(perkX, perkY);
