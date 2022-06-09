b14e000374059db51a6dbe0d71f9e961d01399ca
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2022-06-10 00:47:12 +0200

    Fixed minor bug and minor improvements.

--- src/components/SeedInfo/SeedInfoViews/FungalShifts.tsx
+++ src/components/SeedInfo/SeedInfoViews/FungalShifts.tsx
@@ -7,12 +7,21 @@ import { FungalInfoProvider } from '../../../services/SeedInfo/infoHandler/InfoP
 import { AlchemyConfigContext } from '../../AlchemyConfigContext';
 import { useTranslation } from 'react-i18next';
 import { useMaterialFavorite } from './helpers';
+import { MaterialInfoProvider } from '../../../services/SeedInfo/infoHandler/InfoProviders/Material';
 
 const Flask = () => {
 	return <b className="text-info">Flask</b>;
 };
 
-const FungalMaterial = (props: {materials: Map<string, string>; isFlask: boolean, direction: boolean; isFavorite; showId: boolean}) => {
+interface IFungalMaterialProps {
+	materials: Map<string, string>
+	direction: boolean
+	isFlask: boolean
+	isFavorite // TODO: add type 
+	showId // TODO: add type 
+}
+
+const FungalMaterial = (props: IFungalMaterialProps) => {
 	const key = props.direction ? "to" : "from"
 
 	/*
@@ -41,11 +50,8 @@ const FungalMaterial = (props: {materials: Map<string, string>; isFlask: boolean
 			{props.isFlask && <Flask />}
 			{materialsByNameArray.map(([matName, matIds]) => {
 				return(
-					<div>
-						<div
-							key={`${key}_${matName}`}
-							className={matIds.some(props.isFavorite) && 'text-info'}
-						>
+					<div key={`${matName}-a`}>
+						<div key={`${matName}-b`} className={matIds.some(props.isFavorite) ? 'text-info' : ''}>
 							{capitalize(matName)}
 							{' '}
 							{props.showId && `(${matIds.join(', ')})`}
@@ -63,28 +69,40 @@ interface IShiftProps {
 	shifted: boolean;
 	isFavorite: (id: string) => boolean;
 	setShifted: (shifted: boolean) => void;
+	materialProvider: MaterialInfoProvider;
 	getMaterial: (s: string) => any;
 }
 
 const Shift = (props: IShiftProps) => {
-	const { data, shifted, setShifted, getMaterial, isFavorite } = props;
-
+	const { data, shifted, setShifted, getMaterial, isFavorite, materialProvider } = props;
 	const [showId] = useContext(AlchemyConfigContext);
 
 	// TODO: More uniform if data.from and data.to is always an array?
 	const from: Array<string> = [data.from].flat()
 	const to: Array<string> = [data.to].flat()
 	// TODO: Maybe have getMaterial return already capitalized?
-	const fromMaterials = new Map(from.map(matId => { return [ matId, getMaterial(matId) ]}));
-	const toMaterials = new Map(to.map(matId => { return [ matId, getMaterial(matId) ]}));
+	const fromMaterials = new Map(from.map(matId => { return [ matId, materialProvider.translate(matId) ]}));
+	const toMaterials = new Map(to.map(matId => { return [ matId, materialProvider.translate(matId) ]}));
 
 	return (
 		<tr>
 			<td className="align-middle">
-				<FungalMaterial materials={fromMaterials} isFlask={data.flaskFrom} direction={false} isFavorite={isFavorite} showId={false} />
+				<FungalMaterial
+					materials={fromMaterials}
+					direction={false}
+					isFlask={data.flaskFrom}
+					isFavorite={isFavorite}
+					showId={showId}
+				/>
 			</td>
 			<td className="align-middle">
-				<FungalMaterial materials={toMaterials} isFlask={data.flaskTo} direction={true} isFavorite={isFavorite} showId={false} />
+			<FungalMaterial
+					materials={toMaterials}
+					direction={true}
+					isFlask={data.flaskTo}
+					isFavorite={isFavorite}
+					showId={showId}
+				/>
 			</td>
 			<td>
 				<OverlayTrigger
@@ -138,6 +156,7 @@ const FungalShifts = (props: IFungalShiftsProps) => {
 							isFavorite={isFavorite}
 							shifted={!!infoProvider.config.fungalShifts[i]}
 							setShifted={handleSetShifted(i)}
+							materialProvider={infoProvider.providers.material}
 							getMaterial={s => infoProvider.providers.material.translate(s)}
 						/>
 					);
