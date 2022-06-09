521ad56d9fe0ed5521f7403ceef9a9f6b6695f9d
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2022-06-09 23:12:32 +0200

    Refactored FungalShifts.tsx
    Main improvement is that both, from- and to-materials now use the same component logic.
    
    Minor visual improvements.

--- src/components/SeedInfo/SeedInfoViews/FungalShifts.tsx
+++ src/components/SeedInfo/SeedInfoViews/FungalShifts.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Stack, Form, Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 import GameInfoProvider from '../../../services/SeedInfo/infoHandler';
@@ -6,12 +6,56 @@ import { capitalize } from '../../../services/helpers';
 import { FungalInfoProvider } from '../../../services/SeedInfo/infoHandler/InfoProviders/Fungal';
 import { AlchemyConfigContext } from '../../AlchemyConfigContext';
 import { useTranslation } from 'react-i18next';
-import classNames from 'classnames';
 import { useMaterialFavorite } from './helpers';
 
 const Flask = () => {
 	return <b className="text-info">Flask</b>;
 };
+
+const FungalMaterial = (props: {materials: Map<string, string>; isFlask: boolean, direction: boolean; isFavorite; showId: boolean}) => {
+	const key = props.direction ? "to" : "from"
+
+	/*
+	Var 'props.materials' may contain multiple materials with the same display name.
+	This is because some materials like 'Flammable Gas' have static variants that are
+	pre-rendered in a scene. Static materials have their own material ID.
+
+	These materials should not be displayed twice.
+
+	To solve this, materials will be sorted in a new `Map` where the key is the material
+	name acts as the key and the material ids are stored in the value as an array.
+	*/
+
+	const materialsByName = new Map()
+	props.materials.forEach((matName, matId) => {
+		const ids = materialsByName.get(matName) ?? []
+		ids.push(matId)
+		materialsByName.set(matName, ids)
+	});
+
+	// The logic that returns a JSX component requires an `Array`
+	const materialsByNameArray = Array.from(materialsByName)
+
+	return(
+		<Stack>
+			{props.isFlask && <Flask />}
+			{materialsByNameArray.map(([matName, matIds]) => {
+				return(
+					<div>
+						<div
+							key={`${key}_${matName}`}
+							className={matIds.some(props.isFavorite) && 'text-info'}
+						>
+							{capitalize(matName)}
+							{' '}
+							{props.showId && `(${matIds.join(', ')})`}
+						</div>
+					</div>
+				)
+			})}
+		</Stack>
+	)
+}
 
 interface IShiftProps {
 	key: string | number;
@@ -27,56 +71,22 @@ const Shift = (props: IShiftProps) => {
 
 	const [showId] = useContext(AlchemyConfigContext);
 
-	const From = (props: { materials: string[]; flaskFrom: boolean }) => {
-		const { materials } = props;
-
-		const mats = new Map();
-
-		for (const m of materials) {
-			const displayName = capitalize(getMaterial(m));
-			const arr = mats.get(displayName) || [];
-			arr.push(m);
-			mats.set(displayName, arr);
-		}
-
-		return (
-			<Stack>
-				{data.flaskFrom && <Flask />}
-				{[...mats.keys()].map(displayName => {
-					const ids = mats.get(displayName);
-					return (
-						<div
-							key={displayName}
-							className={classNames(ids.some(isFavorite) && 'text-info')}
-						>
-							{displayName} {showId && `(${ids.join(', ')})`}
-						</div>
-					);
-				})}
-			</Stack>
-		);
-	};
+	// TODO: More uniform if data.from and data.to is always an array?
+	const from: Array<string> = [data.from].flat()
+	const to: Array<string> = [data.to].flat()
+	// TODO: Maybe have getMaterial return already capitalized?
+	const fromMaterials = new Map(from.map(matId => { return [ matId, getMaterial(matId) ]}));
+	const toMaterials = new Map(to.map(matId => { return [ matId, getMaterial(matId) ]}));
 
 	return (
-		<tr
-			className={classNames([
-				// (data.flaskTo || data.flaskFrom) && 'text-body',
-				// !(data.flaskTo || data.flaskFrom) && 'text-muted',
-				// ''
-			])}
-		>
-			<td className={classNames('col-auto')}>
-				<From materials={data.from} flaskFrom={data.flaskFrom} />
+		<tr>
+			<td className="align-middle">
+				<FungalMaterial materials={fromMaterials} isFlask={data.flaskFrom} direction={false} isFavorite={isFavorite} showId={false} />
 			</td>
-			<td className={classNames('col-auto')}>
-				<Stack>
-					{data.flaskTo && <Flask />}
-					<div className={classNames(isFavorite(data.to) && 'text-info')}>
-						{capitalize(getMaterial(data.to))} {showId && `(${data.to})`}
-					</div>
-				</Stack>
+			<td className="align-middle">
+				<FungalMaterial materials={toMaterials} isFlask={data.flaskTo} direction={true} isFavorite={isFavorite} showId={false} />
 			</td>
-			<td className="col-auto">
+			<td>
 				<OverlayTrigger
 					placement="right"
 					key="right"
@@ -118,7 +128,7 @@ const FungalShifts = (props: IFungalShiftsProps) => {
 	};
 
 	return (
-		<table className="table table-sm align-middle table-responsive">
+		<table className="table table-sm table-responsive">
 			<tbody>
 				{fungalData.map((data, i) => {
 					return (
