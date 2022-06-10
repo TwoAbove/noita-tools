731611c7b2846da9edede1de1ce80ecef582383b
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2022-06-10 14:56:58 +0200

    Fixes #224

--- src/components/Icons/Clickable.tsx
+++ src/components/Icons/Clickable.tsx
@@ -25,9 +25,9 @@ const Clickable = (props: IClickableProps | any) => {
 		setHovered(false);
 	};
 
-	const clickedStyles = clicked && 'border border-3 p-1 flex-shrink-1';
-	// TODO need to get centering correct so that there is not jittering
-	const hoverStyles = false && hovered && 'border';
+	// Clicked-style is prioritised, then hovered-style, otherwise fallback to default style.
+	const clickedOrHoveredStyle = clicked ? 'bg-info' : hovered ? 'bg-light' : 'bg-body'
+
 	return (
 		<div
 			onMouseEnter={handleMouseEnter}
@@ -35,7 +35,7 @@ const Clickable = (props: IClickableProps | any) => {
 			style={{
 				cursor: 'pointer'
 			}}
-			className={classNames('', clickedStyles, hoverStyles)}
+			className={classNames('p-1', clickedOrHoveredStyle)}
 		>
 			{childrenWithProps}
 		</div>
