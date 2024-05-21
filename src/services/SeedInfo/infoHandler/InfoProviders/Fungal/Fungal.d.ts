3ab6234d17b5b64946eeafec6c034f7e7d2927f7
Seva Maltsev <me@seva.dev>
2024-05-21 10:47:55 -0400

    Upgraded to latest emscripten

--- src/services/SeedInfo/infoHandler/InfoProviders/Fungal/Fungal.d.ts
+++ src/services/SeedInfo/infoHandler/InfoProviders/Fungal/Fungal.d.ts
@@ -1,18 +1,29 @@
-export interface VectorString {
-  size(): number;
-  push_back(_0: ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string): void;
-  resize(_0: number, _1: ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string): void;
-  set(_0: number, _1: ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string): boolean;
-  get(_0: number): any;
-  delete(): void;
+// TypeScript bindings for emscripten-generated code.  Automatically generated at compile time.
+declare namespace RuntimeExports {
+    let HEAPF32: any;
+    let HEAPF64: any;
+    let HEAP_DATA_VIEW: any;
+    let HEAP8: any;
+    let HEAPU8: any;
+    let HEAP16: any;
+    let HEAPU16: any;
+    let HEAP32: any;
+    let HEAPU32: any;
+    let HEAP64: any;
+    let HEAPU64: any;
+}
+interface WasmModule {
+  _malloc(_0: number): number;
+  _free(_0: number): void;
 }
 
-export interface VectorFungalTransformation {
+type EmbindString = ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string;
+export interface VectorString {
+  push_back(_0: EmbindString): void;
+  resize(_0: number, _1: EmbindString): void;
   size(): number;
-  push_back(_0: FungalTransformation): void;
-  resize(_0: number, _1: FungalTransformation): void;
-  set(_0: number, _1: FungalTransformation): boolean;
-  get(_0: number): any;
+  get(_0: number): EmbindString | undefined;
+  set(_0: number, _1: EmbindString): boolean;
   delete(): void;
 }
 
@@ -20,13 +31,25 @@ export type FungalTransformation = {
   flaskTo: boolean,
   flaskFrom: boolean,
   from: VectorString,
-  to: ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string,
-  gold_to_x: ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string,
-  grass_to_x: ArrayBuffer|Uint8Array|Uint8ClampedArray|Int8Array|string
+  to: EmbindString,
+  gold_to_x: EmbindString,
+  grass_to_x: EmbindString
 };
 
-export interface MainModule {
+export interface VectorFungalTransformation {
+  push_back(_0: FungalTransformation): void;
+  resize(_0: number, _1: FungalTransformation): void;
+  size(): number;
+  get(_0: number): FungalTransformation | undefined;
+  set(_0: number, _1: FungalTransformation): boolean;
+  delete(): void;
+}
+
+interface EmbindModule {
   VectorString: {new(): VectorString};
   VectorFungalTransformation: {new(): VectorFungalTransformation};
   PickForSeed(_0: number, _1: number): VectorFungalTransformation;
 }
+
+export type MainModule = WasmModule & typeof RuntimeExports & EmbindModule;
+export default function MainModuleFactory (options?: unknown): Promise<MainModule>;
