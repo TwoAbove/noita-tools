f1b33543d3f7d8347273b4e0534766c9586bb95f
Seva Maltsev <me@seva.dev>
2024-07-14 12:07:51 -0400

    Updated lc and ap materials

--- src/services/SeedInfo/infoHandler/InfoProviders/Alchemy/alchemy.cpp
+++ src/services/SeedInfo/infoHandler/InfoProviders/Alchemy/alchemy.cpp
@@ -12,17 +12,17 @@ public:
       "magic_liquid_faster_levitation", "magic_liquid_faster_levitation_and_movement", "magic_liquid_invisibility",
       "magic_liquid_mana_regeneration", "magic_liquid_movement_faster", "magic_liquid_protection_all",
       "magic_liquid_teleportation", "magic_liquid_unstable_polymorph", "magic_liquid_unstable_teleportation",
-      "magic_liquid_worm_attractor", "material_confusion", "mud",
-      "oil", "poison", "radioactive_liquid_yellow",
+      "magic_liquid_worm_attractor", "confusion", "mud",
+      "oil", "poison", "radioactive_liquid",
       "swamp", "urine", "water",
       "water_ice", "water_swamp", "magic_liquid_random_polymorph"};
 
   vector<string> ALCHEMY = {
-      "bone_box2d", "brass", "coal",
+      "bone", "brass", "coal",
       "copper", "diamond", "fungi",
       "gold", "grass", "gunpowder",
-      "gunpowder_explosive", "rotten_meat", "sand_petrify",
-      "silver", "slime", "snow_b2",
+      "gunpowder_explosive", "rotten_meat", "sand",
+      "silver", "slime", "snow",
       "soil", "wax", "honey"};
 
   NollaPrng *PRNG;
